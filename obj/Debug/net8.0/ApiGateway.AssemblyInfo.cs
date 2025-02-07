8e211ab7c7148043ad35d426eee649ce87d1ce7b
Anggelo <anstalyn1999@gmail.com>
2025-02-07 15:06:18 -0500

    feat: :construction_worker: Update the files ApiGatawey

--- obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19f743bd6fd608e39658cda56618c4f0a139f75a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2b38b18353122e031cba90a787004d7c54183b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
