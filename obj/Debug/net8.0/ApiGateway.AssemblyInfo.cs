0d80f1a00eca6fa379fe76964a3c578a78e57d84
Anggelo <anstalyn1999@gmail.com>
2025-02-07 22:20:31 -0500

    feat: :wrench: ApiGateway file configuration

--- obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e211ab7c7148043ad35d426eee649ce87d1ce7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78395ba1a7916281681b7c4e46987cdde37694ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
