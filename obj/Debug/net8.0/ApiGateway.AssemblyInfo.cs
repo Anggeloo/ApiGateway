7e8ab86df569cac3f95cfab4bc13784a21720f53
Anggelo <anstalyn1999@gmail.com>
2025-02-11 19:55:48 -0500

    feat: :wrench: Rebuild the project

--- obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c65e39f8b554a53facb3322b8a80f6b4a5243ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97bd9a5acf7a5970fc31f1f0f147440e73d92f8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
